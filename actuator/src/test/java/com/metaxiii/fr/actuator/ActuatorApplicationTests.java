c56cfb31deb6ee23de1420c0d066dcedfb5ee887
metaxiii <lehchibi.gael@pm.me>
2022-11-05 18:21:52 +0100

    feat: add actuator info

---
+++ actuator/src/test/java/com/metaxiii/fr/actuator/ActuatorApplicationTests.java
@@ -0,0 +1,30 @@
+package com.metaxiii.fr.actuator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ActuatorApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void contextLoads() {
+        Assertions.assertDoesNotThrow(() -> ActuatorApplication.main(new String[]{}));
+    }
+
+    @Test
+    void itShouldCallActuator() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/actuator")).andDo(print()).andExpect(status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.get("/actuator/info")).andDo(print()).andExpect(status().isOk());
+    }
+}
