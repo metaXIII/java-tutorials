62cfd60d0836dda606b9d0c2897b0c8f181e7b1f
metaxiii <16118999+metaXIII@users.noreply.github.com>
2026-05-12 16:14:28 +0200

    nothing

--- actuator/src/main/java/com/metaxiii/fr/actuator/ActuatorApplication.java
+++ actuator/src/main/java/com/metaxiii/fr/actuator/ActuatorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ActuatorApplication {
 
-  static void main(final String[] args) {
+  public static void main(final String[] args) {
     SpringApplication.run(ActuatorApplication.class, args);
   }
 }
